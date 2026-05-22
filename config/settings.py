93bfb64ba1fd4211af59d350784893c69f4a63d5
Yevhen <yevhen.huriev@hneu.net>
2026-05-22 14:56:31 +0300

    fix whitenoise root

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -93,6 +94,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 STATICFILES_MANIFEST_STRICT = False
+WHITENOISE_ROOT = BASE_DIR / 'static'
 
 
 MEDIA_URL = '/media/'
