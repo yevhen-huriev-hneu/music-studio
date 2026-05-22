e8d9cd3aff8d39be4ba5442f5213f30161263a6f
Yevhen <yevhen.huriev@hneu.net>
2026-05-22 14:18:49 +0300

    fix whitenoise storage

--- config/settings.py
+++ config/settings.py
@@ -92,7 +92,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
