f0e8d0ce9ac1f11de0ae33a488075d2d5294bb66
Yevhen <yevhen.huriev@hneu.net>
2026-05-22 15:06:38 +0300

    fix whitenoise root

--- config/settings.py
+++ config/settings.py
@@ -7,6 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='http://localhost:8000').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
